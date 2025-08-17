9b3b045ab0705b0983d52dd5b9f7d5b1ee1d698f
Ronse11 <seronronalddalida@gmail.com>
2025-08-17 16:52:06 +0800

    style: updated layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='bg-primary'>
-      <div className="max-w-standard mx-auto lg:p-20 md:p-16 sm:p-8 p-4">
+      <div className="max-w-standard mx-auto lg:p-20 md:p-16 sm:p-8 p-8">
         <div className="flex flex-col sm:flex-row justify-between gap-8">
           <div>
-            <h2 className="text-white md:text-[2.5rem] text-3xl font-light mb-6 sm:text-left text-center">Let's Connect</h2>
-            <p className="text-white/80 mb-8 max-w-md md:text-base text-sm sm:text-left text-center">
+            <h2 className="text-white md:text-[2.5rem] text-3xl font-light mb-6 text-left">Let's Connect</h2>
+            <p className="text-white/80 mb-8 max-w-md md:text-base text-sm text-balance">
               I'm actively seeking junior web developer opportunities. 
               Let's discuss how I can contribute to your team!
             </p>
-            <ul className='flex sm:justify-start justify-center md:gap-8 gap-6 text-white text-sm font-medium'>
+            <ul className='flex justify-start md:gap-8 gap-6 text-white text-sm font-medium'>
               <li className='hover:bg-button rounded-full transition-colors duration-300'>
                 <a href="#hero" className='block '>Home</a>
               </li>
@@ -24,12 +24,12 @@ const Footer = () => {
             </ul>
           </div>
           
-          <div className="md:text-right text-center">
+          <div className="text-left max-sm:border-t border-white/20 max-sm:pt-8">
             {/* <h3 className='text-white text-[3rem] font-light mb-4'>ronald@gmail.com</h3> */}
-            <div className="flex flex-col gap-3 text-white/80 md:text-base text-xs">
+            <div className="flex flex-col gap-3 text-white/80 md:text-lg text-sm">
               <a 
-                href="mailto:ronald@gmail.com" 
-                className="hover:text-white transition-colors duration-300 text-lg"
+                href="mailto:seronronalddalida@gmail.com" 
+                className="hover:text-white transition-colors duration-300 "
               >
                 📧 seronronalddalida@gmail.com
               </a>
@@ -49,9 +49,9 @@ const Footer = () => {
               >
                 💻 GitHub Profile
               </a>
-              <span className="text-sm">
+              {/* <span className="text-sm">
                 📍 Available for opportunities worldwide (Open to relocation)
-              </span>
+              </span> */}
             </div>
           </div>
         </div>
