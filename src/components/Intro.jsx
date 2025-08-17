6a8f0e23f06db44386d5e880e5f49f8872af104e
Ronse11 <seronronalddalida@gmail.com>
2025-08-17 16:53:58 +0800

    style: updated layout

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,5 +1,7 @@
 import React, { useRef } from 'react'
 import useFadeInOnScroll from '../hooks/useFadeInOnScroll';
+import github from "../assets/github.png";
+import gmail from "../assets/gmail.png";
 
 const Intro = () => {
     const fadeRef1 = useRef(null);
@@ -8,21 +10,28 @@ const Intro = () => {
     
     return (
         <div className=' max-w-standard mx-auto lg:px-12 md:px-8 sm:px-4 px-3 bg-transparent'>
-            <div className='w-full flex items-center bg-white'>
-                <div ref={fadeRef1} className='flex-1 flex flex-col items-center justify-center py-18 gap-4 h-full bg-tertiary fade-up border border-line px-3'>
-                    <div className='text-base sm:text-center text-start w-full text-primary'>
-                        <h1 className=''>(Email me to Collaborate)</h1>
+            <div className='w-full flex items-center bg-tertiary border border-line px-8 py-8'>
+                <div ref={fadeRef1} className='flex-1 flex max-sm:flex-col gap-8 fade-up text-xl font-light'>
+                    <div className='flex-1 flex justify-center items-center py-8 gap-2 outline outline-line bg-white'>
+                        <img src={github} alt="" 
+                        className='w-10'/>
+                        <h1>Github</h1>
                     </div>
-                    <div className='md:text-5xl text-3xl sm:text-center text-start font-medium max-w-5xl text-primary'>
-                        <h1>Fresh Graduate with Proven Web Development Skills, Ready to Compete and Collaborate!</h1>
+                    <div className='flex-1 flex justify-center items-center py-8 gap-2 outline outline-line bg-white'>
+                        <img src={gmail} alt="" 
+                        className='w-10'/>
+                        <h1>Gmail</h1>
                     </div>
-                    <div className='md:text-base text-sm sm:text-center text-start max-w-2xl text-secondary'>
-                        <p>With hands-on freelancing experience and a passion for clean, scalable code, I’m prepared to adapt fast and contribute immediately.</p>
+                    <div className='flex-1 flex justify-center items-center py-8 gap-2 outline outline-line bg-white'>
+                        <img src={github} alt="" 
+                        className='w-10'/>
+                        <h1>Github</h1>
                     </div>
-                    <a href="" className='w-full flex sm:justify-center justify-start items-center mt-4'>
-                        <h1 className='border-b-2 border-primary'>Email Me</h1>
-                        <i className='bx  bx-arrow-up-right-stroke text-xl'  ></i> 
-                    </a>
+                    <div className='flex-1 flex justify-center items-center py-8 gap-2 outline outline-line bg-white'>
+                        <img src={gmail} alt="" 
+                        className='w-10'/>
+                        <h1>Gmail</h1>
+                    </div>
                 </div>
             </div>
         </div>
