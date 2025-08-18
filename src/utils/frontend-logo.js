e9a619dca0e78e2c2bcab3e4f05883c7c5cc8364
Ronse11 <seronronalddalida@gmail.com>
2025-08-18 16:42:34 +0800

    refactor(utils): centralize image imports in a single file

---
+++ src/utils/frontend-logo.js
@@ -0,0 +1,21 @@
+import bootstrap from '../assets/skills/frontend/bootstrap.svg';
+import css3 from '../assets/skills/frontend/css3.svg';
+import html5 from '../assets/skills/frontend/html5.svg';
+import javascript from '../assets/skills/frontend/javascript.svg';
+import react from '../assets/skills/frontend/react.svg';
+import tailwind from '../assets/skills/frontend/tailwind.svg';
+import vitejs from '../assets/skills/frontend/vitejs.svg';
+import vue from '../assets/skills/frontend/vue.svg';
+
+const frontendLogo = [
+    { image: html5, name: "HTML5" },
+    { image: css3, name: "CSS3" },
+    { image: javascript, name: "JavaScript" },
+    { image: react, name: "React" },
+    { image: vue, name: "Vue" },
+    { image: tailwind, name: "TailwindCSS" },
+    { image: bootstrap, name: "Bootstrap" },
+    { image: vitejs, name: "Vite" },
+];
+
+export default frontendLogo;
