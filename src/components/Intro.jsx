f8820b0cce054d8260e635f47ed21841ba48f458
Ronse11 <seronronalddalida@gmail.com>
2025-08-18 16:33:34 +0800

    style: adjusted padding and gap

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,37 +1,35 @@
 import React, { useRef } from 'react'
 import useFadeInOnScroll from '../hooks/useFadeInOnScroll';
-import github from "../assets/github.png";
-import gmail from "../assets/gmail.png";
+import github from "../assets/socials/github.png";
+import gmail from "../assets/socials/gmail.png";
+import linkedin from "../assets/socials/linkedin.png";
 
 const Intro = () => {
     const fadeRef1 = useRef(null);
 
     useFadeInOnScroll(fadeRef1);
+
+    const socials = [
+        { image: gmail, name: "Gmail" },
+        { image: github, name: "Github" }, 
+        { image: linkedin, name: "LinkedIn" },
+    ];
     
     return (
         <div className=' max-w-standard mx-auto lg:px-12 md:px-8 sm:px-4 px-3 bg-transparent'>
-            <div className='w-full flex items-center bg-tertiary border border-line px-8 py-8'>
-                <div ref={fadeRef1} className='flex-1 flex max-sm:flex-col gap-8 fade-up text-xl font-light'>
-                    <div className='flex-1 flex justify-center items-center py-8 gap-2 outline outline-line bg-white'>
-                        <img src={github} alt="" 
-                        className='w-10'/>
-                        <h1>Github</h1>
-                    </div>
-                    <div className='flex-1 flex justify-center items-center py-8 gap-2 outline outline-line bg-white'>
-                        <img src={gmail} alt="" 
-                        className='w-10'/>
-                        <h1>Gmail</h1>
-                    </div>
-                    <div className='flex-1 flex justify-center items-center py-8 gap-2 outline outline-line bg-white'>
-                        <img src={github} alt="" 
-                        className='w-10'/>
-                        <h1>Github</h1>
-                    </div>
-                    <div className='flex-1 flex justify-center items-center py-8 gap-2 outline outline-line bg-white'>
-                        <img src={gmail} alt="" 
-                        className='w-10'/>
-                        <h1>Gmail</h1>
-                    </div>
+            <div className='w-full flex items-center bg-tertiary border border-line p-1'>
+                <div ref={fadeRef1} className='w-full text-xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1 font-light'>
+                    {socials.map((src, index) => (
+                        <div key={index} className='flex min-w-[200px] h-32 justify-center items-center gap-2 bg-white outline outline-line rounded-sm'>
+                            <img src={src.image} alt={`img-${index}`} 
+                            className='w-10'/>
+                            <h1>{src.name}</h1>
+                        </div>
+                    ))}
+                        <div className='flex min-w-[200px] h-32 justify-center items-center gap-2 bg-white outline outline-line rounded-sm'>
+                            <i className='bx bxs-phone text-4xl'></i> 
+                            <h1>Number</h1>
+                        </div>
                 </div>
             </div>
         </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import frontendLogo from "../utils/frontend-logo.js";
 
 const Skills = () => {
+
   return (
-    <div>Skills</div>
+    <div className=' max-w-standard mx-auto lg:px-12 md:px-8 sm:px-4 px-3 bg-transparent'>
+        <div className='w-full flex items-center bg-tertiary border border-line p-1'>
+            <div className="w-full text-xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1 font-light">
+            {frontendLogo.map((logo, index) => (
+                <div
+                key={index}
+                className="flex min-w-[200px] h-32 justify-center items-center gap-2 bg-white outline outline-line rounded-sm"
+                >
+                <img src={logo.image} alt={`img-${index}`} className="w-10" />
+                <h1>{logo.name}</h1>
+                </div>
+            ))}
+            </div>
+        </div>
+    </div>
   )
 }
 
