271d297828a68b480fae9ab37fa7e501dbc605f5
Ronse11 <seronronalddalida@gmail.com>
2025-08-20 15:35:36 +0800

    style: updated the layout and padding

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,13 +5,13 @@ const Skills = () => {
   return (
     <div className=' max-w-standard mx-auto lg:px-12 md:px-8 sm:px-4 px-3 bg-transparent'>
         <div className='w-full flex items-center bg-tertiary border border-line p-1'>
-            <div className="w-full text-xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1 font-light">
+            <div className="w-full sm:text-xl text-sm grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1 font-light">
             {frontendLogo.map((logo, index) => (
                 <div
                 key={index}
-                className="flex min-w-[200px] h-32 justify-center items-center gap-2 bg-white outline outline-line rounded-sm"
+                className="flex max-sm:flex-col sm:min-w-[200px] min-w-[100px] sm:h-32 h-24 justify-center items-center gap-2 bg-white outline outline-line rounded-sm"
                 >
-                <img src={logo.image} alt={`img-${index}`} className="w-10" />
+                <img src={logo.image} alt={`img-${index}`} className="sm:w-10 w-7" />
                 <h1>{logo.name}</h1>
                 </div>
             ))}
