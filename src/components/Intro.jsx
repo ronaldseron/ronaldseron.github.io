4023be46aac3005b9ab3924f23c7068e2ff65fba
Ronse11 <seronronalddalida@gmail.com>
2025-09-21 17:13:09 +0800

    chore: removed animation

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -32,7 +32,7 @@ const Intro = () => {
                 
                     <div className='sm:min-w-[200px] min-w-[100px] max-sm:h-24 hover:z-40 z-30 cursor-pointer hover:-translate-y-0.5 hover:scale-105 hover:shadow-2xl duration-300 transition-transform'>
                         <motion.div
-                        {...scrollAnimations.card(4)}
+                        // {...scrollAnimations.card(4)}
                         className='h-full bg-white flex max-md:flex-col-reverse sm:p-8 sm:justify-between justify-center items-center gap-2  outline outline-line rounded-sm'>
                             <div className='sm:h-full'>
                                 <h1>Number</h1>
